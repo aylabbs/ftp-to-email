1913a32377eb36794ac8a63cc4def529acb50315
unknown <aylabbs@gmail.com>
2021-05-31 10:51:55 -0400

    email config from env

--- src/fs.js
+++ src/fs.js
@@ -2,23 +2,26 @@ const FileSystem = require('ftp-srv/src/fs');
 const { PassThrough } = require('stream')
 const transporter = require('./mail-transport')
 
+const { EMAIL_FROM, EMAIL_TO } = process.env
+
+if (!EMAIL_FROM || !EMAIL_TO) throw new Error('Missing email FROM/TO')
+
 class MyFileSystem extends FileSystem {
   constructor() {
     super(...arguments);
   }
 
   write(fileName, {append = false, start = undefined} = {}) {
-    console.log(fileName)
+    
     const ptStream = new PassThrough()
-      // send mail with defined transport object
     transporter.sendMail({
-      from: '"Fred Foo 👻" <foo@example.com>', // sender address
-      to: "aylabbs@gmail.com", // list of receivers
-      subject: "Hello ✔", // Subject line
-      text: "Hello world?", // plain text body
-      html: "<b>Hello world?</b>", // html body
+      from: EMAIL_FROM,
+      to:  EMAIL_TO,
+      subject: "Scan " + fileName,
+      text: "",
+      // html: "<b>Hello world?</b>",
       attachments: [
-        { // stream as an attachment
+        {
           filename: fileName,
           content: ptStream
         }
